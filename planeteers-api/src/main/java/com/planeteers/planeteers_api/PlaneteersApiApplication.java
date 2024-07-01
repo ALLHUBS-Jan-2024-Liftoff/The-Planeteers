2d6e2592e79b76e8a35fe78658086bf37ea0edda
Jeremy <thisismejeremy@gmail.com>
2024-07-01 18:05:36 -0400

    added api

---
+++ planeteers-api/src/main/java/com/planeteers/planeteers_api/PlaneteersApiApplication.java
@@ -0,0 +1,13 @@
+package com.planeteers.planeteers_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneteersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneteersApiApplication.class, args);
+	}
+
+}
---
+++ planeteers-api/src/test/java/com/planeteers/planeteers_api/PlaneteersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planeteers.planeteers_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneteersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
