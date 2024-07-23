dd9ee6aaa75f00d0a39cf80cb57945eee18c631d
kalille Overton <>
2024-07-23 12:10:05 -0400

    added getUserById and updateUser to UserService and functionality to UserServiceImpl

--- planeteers-api/src/main/java/com/planeteers/planeteers_api/service/UserServiceImpl.java
+++ planeteers-api/src/main/java/com/planeteers/planeteers_api/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -21,5 +22,31 @@ public class UserServiceImpl implements UserService{
     @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public Optional<User> updateUser(int id, User user) {
+        Optional<User> optUser = userRepository.findById(id);
+        if (!optUser.isPresent()) {
+            return Optional.empty();
+        }
+        try {
+            User currentUser = optUser.get();
+            currentUser.setName(user.getName());
+            currentUser.setAge(user.getAge());
+            currentUser.setEmail(user.getEmail());
+            if (user.getPwHash() != null && !user.getPwHash().isEmpty()) {
+                currentUser.setPwHash(user.getPwHash());
+            }
+            userRepository.save(currentUser);
+            return Optional.of(currentUser);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 }
