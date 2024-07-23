2d449ca55be7a6e789d8ff63b6f9af60190777d3
Jeremy <thisismejeremy@gmail.com>
2024-07-23 15:39:10 -0400

    Reverted changes as they were made in the wrong branch

--- planeteers-ui/src/pages/Home.jsx
+++ planeteers-ui/src/pages/Home.jsx
@@ -13,10 +13,10 @@ export default function Home() {
             </ul>
         </div>
        	<div class="games">
-          	<div><img src="https://www.dropbox.com/scl/fi/bmlqqtls5mhd2ti806w29/cardmatch.jpg?rlkey=2l1c8rwwkrx49eorp3z1qh53y&st=cik849uh&raw=1" alt="Card Match" /></div>
-  			<div><img src="https://www.dropbox.com/scl/fi/ca6lpj5pg37bj26pp3ful/gameofwar.jpg?rlkey=pfwxuigp06ggy76u8wpnnx82d&st=0g6gd19b&raw=1" alt="Game of War" /></div>
- 			<div><img src="https://www.dropbox.com/scl/fi/4y8r6k98508dk5z1rhfop/solitaire.png?rlkey=wgvpgv32pwtg5rl1baj4s0wul&st=f58z71ki&raw=1" alt="Solitaire" /></div>
-  			<div><img src="https://www.dropbox.com/scl/fi/93epmz1pc6amh6g1dwrvw/blackjack.jpg?rlkey=jwhpa1cfqaoaw8nwdjaxfah4t&st=wun5jgco&raw=1" alt="Blackjack" /></div>
+          	<div>Game 1</div>
+  			<div>Game 2</div>
+ 			<div>Game 3</div>
+  			<div>Game 4</div>
         </div>
         <div class = "footer">
             <p>Contact Us</p>
