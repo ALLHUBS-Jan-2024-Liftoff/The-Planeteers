7c30a68bb84cc8828ee24d52ac249888dc9315ce
kalille Overton <>
2024-08-02 18:50:53 -0400

    created comment controller and Services all actions were tested and passed

--- planeteers-api/src/main/java/com/planeteers/planeteers_api/models/data/CommentRepository.java
+++ planeteers-api/src/main/java/com/planeteers/planeteers_api/models/data/CommentRepository.java
@@ -1,9 +1,10 @@
 package com.planeteers.planeteers_api.models.data;
 
 import com.planeteers.planeteers_api.models.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends CrudRepository <Comment, String> {
+public interface CommentRepository extends JpaRepository <Comment, Integer> {
 }
