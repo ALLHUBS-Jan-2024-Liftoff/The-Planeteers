87e0f7e6b2dcf67413d130205d159f215f545048
Manuel Ardon <maardon09@gmail.com>
2024-08-12 18:43:13 -0400

    added files Content, Cover, Main, Sidebar using Chakra libraries

--- planeteers-ui/src/App.jsx
+++ planeteers-ui/src/App.jsx
@@ -4,22 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Login } from './components/Users/Login'
 import { Register } from './components/Users/Register'
+import SimpleBar from 'simplebar-react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from 'helpers'
+import Cover from 'components/Cover'
+import Main from 'components/Main'
 
 
-function App() {
-  const [currentForm, setCurrentForm] = useState('login')
-
-  const toggleForm = (formFirstName, formLastName) => {
-    setCurrentForm (formFirstName, formLastName);
-  }
-
+export default function App() {
   return (
-   <div className='App'>
-    {
-      currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
-    }
-   </div>
-  );
+    <SimpleBar style={{ maxHeight: '100vh' }}>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Cover />
+              <Main />
+            </Route>
+          </Switch>
+        </Router>
+      </ChakraProvider>
+    </SimpleBar>
+  )
 }
 
-export default App;
